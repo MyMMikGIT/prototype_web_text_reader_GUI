3f385d9ed7ad04a58b6947bd53c3072251c35922
MyMMikGIT <mmikolajunas@yahoo.com>
2017-01-06 14:21:57 +0200

    Create translator_v2.py

---
+++ translator_v2.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+
+import io
+import common_modules as c_m
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
